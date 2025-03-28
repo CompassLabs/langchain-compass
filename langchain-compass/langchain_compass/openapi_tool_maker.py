3fb0754a993d5c803c444968eb6370c86f8c3da4
Lukas Scheucher <scheuclu@gmail.com>
2025-03-28 15:37:37 +0000

    Added API key.

--- langchain-compass/langchain_compass/openapi_tool_maker.py
+++ langchain-compass/langchain_compass/openapi_tool_maker.py
@@ -1,7 +1,29 @@
+import importlib
+
+###########
+import json
+import os
+from functools import lru_cache
 from typing import Callable, List, Optional
 
+import requests
 from langchain_core.tools import BaseTool
 from pydantic._internal._model_construction import ModelMetaclass
+
+from langchain_compass.params_converter import generate_pydantic_model
+from langchain_compass.rest_get_maker import make_get_tool
+from langchain_compass.rest_post_maker import make_post_tool
+
+
+@lru_cache(maxsize=None)
+def _get_request_cached(openapi_json_address: str) -> str:
+    return requests.get(openapi_json_address).text
+
+
+def snake_to_camel(snake_str: str) -> str:
+    return "".join(
+        word[:1].upper() + word[1:] for word in snake_str.strip("_").split("_")
+    )
 
 
 def make_tools(
@@ -9,4 +31,93 @@ def make_tools(
     api_key: Optional[str],
     func_check_direct_return: Callable[[ModelMetaclass], bool],
 ) -> List[BaseTool]:
-    return []
+    try:
+        with open("temp.json", "w") as apifile:
+            apifile.write(_get_request_cached(openapi_json_address))
+        with open("temp.json", "r") as file:
+            openapi_data = json.load(file)
+        os.system(
+            "datamodel-codegen --input temp.json --input-file-type \
+            openapi --output-model-type pydantic_v2.BaseModel \
+            --openapi-scopes=schemas  --output schemas.py "
+        )
+        import schemas  # type: ignore
+
+        importlib.reload(schemas)
+        # importlib.reload(paths)
+    except:  # noqa: E722
+        raise Exception(f"Failed to generate tools for {openapi_json_address}")
+
+    def get_response_schema_name(endpoint: dict) -> str:
+        return endpoint["responses"]["200"]["content"]["application/json"]["schema"][
+            "$ref"
+        ].split("/")[-1]
+
+    tools = []
+    for path in openapi_data["paths"].keys():
+        if "patch" in openapi_data["paths"][path]:
+            raise ValueError("We don't support patch requests yet.")
+        if "update" in openapi_data["paths"][path]:
+            raise ValueError("We don't support update requests yet.")
+
+        if "post" in openapi_data["paths"][path]:
+            endpoint = openapi_data["paths"][path]["post"]
+
+            schema_name = endpoint["requestBody"]["content"]["application/json"][
+                "schema"
+            ]["$ref"].split("/")[-1]
+
+            response_schema_name = get_response_schema_name(endpoint)
+            tool_name = path.replace("/v0/", "").replace("/", "_") + "_"  # TODO
+            response_type = getattr(schemas, snake_to_camel(response_schema_name))
+            tools.append(
+                make_post_tool(
+                    url1=openapi_data["servers"][0]["url"].rstrip("/")
+                    + "/"
+                    + path.lstrip("/"),  # ugly but gets the job done
+                    name1=tool_name,
+                    description1=endpoint["description"]
+                    if "description" in endpoint
+                    else endpoint["summary"],
+                    args_schema1=getattr(schemas, schema_name.replace("_", "")),
+                    return_direct1=False
+                    if func_check_direct_return is None
+                    else func_check_direct_return(response_type),
+                    response_type1=response_type,
+                )()
+            )
+        if "get" in openapi_data["paths"][path]:
+            endpoint = openapi_data["paths"][path]["get"]
+
+            response_schema_name = get_response_schema_name(endpoint)
+            tool_name = path.replace("/v0/", "").replace("/", "_") + "_"  # TODO
+
+            if "parameters" in endpoint:
+                # for parameter in endpoint['parameters']:
+                #     print(parameter)
+                #     print(parameter['schema'])
+                Params = generate_pydantic_model(
+                    model_name="Params", parameters=endpoint["parameters"]
+                )
+            else:
+                Params = None
+
+            tools.append(
+                make_get_tool(
+                    url1=openapi_data["servers"][0]["url"].rstrip("/")
+                    + "/"
+                    + path.lstrip("/"),  # ugly but gets the job done
+                    name1=tool_name,
+                    description1=endpoint["description"]
+                    if "description" in endpoint
+                    else endpoint["summary"],
+                    args_schema1=Params,
+                    return_direct1=False,
+                    response_type1=getattr(
+                        schemas, response_schema_name.replace("_", "")
+                    ),
+                )()
+            )
+
+    os.remove("schemas.py")
+    return tools
---
+++ langchain-compass/langchain_compass/rest_post_maker.py
@@ -0,0 +1,54 @@
+from typing import Any, Optional, Type
+
+import requests
+from langchain_core.callbacks import (
+    CallbackManagerForToolRun,
+)
+from langchain_core.tools import BaseTool
+from pydantic import BaseModel
+
+
+def make_post_tool(
+    name1: str,
+    description1: str,
+    args_schema1: Type[BaseModel],
+    return_direct1: bool,
+    url1: str,
+    response_type1: type,
+    api_key: Optional[str] = None,
+) -> type[BaseTool]:
+    class PostRequestTool(BaseTool):
+        name: str = name1
+        description: str = description1
+        url: str = url1
+        args_schema: Type[BaseModel] = args_schema1
+        return_direct: bool = return_direct1
+        verbose: bool = True
+        response_type: Any = response_type1
+
+        def _run(
+            self,
+            run_manager: CallbackManagerForToolRun | None = None,
+            **kwargs: Any,
+        ) -> response_type1:  # type: ignore
+            """Use the tool."""
+
+            headers = {"accept": "application/json", "Content-Type": "application/json"}
+            if api_key is not None:
+                headers["x-api-key"] = api_key
+
+            response = requests.post(
+                self.url,
+                json=self.args_schema(**kwargs).model_dump(mode="json"),
+                headers=headers,
+            )
+            if response.status_code != 200:
+                raise Exception(response.text)
+            if self.return_direct:  # TODO
+                if "image" in response.json():
+                    return {"type": "image", "content": response.json()["image"]}
+                return {"type": "unsigned_transaction", "content": response.json()}
+            return self.response_type(**response.json())
+
+    PostRequestTool.__name__ = name1
+    return PostRequestTool
